094cb64c303e6cde33233419b015a1007e1d8edf
João Henrique <santos.joao@crmsolucoes.com.br>
2023-08-25 12:55:45 -0300

    5 - Nuvem

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 window.onload = function(){
 
-    document.querySelector('.moeda').setAttribute('checked', 'checked')
+    //Ao carregar a tela, adiciona o atributo "checked" ao elemento ".moeda"
+
+    document.querySelector('.moeda').setAttribute('checked', 'checked');
+
+    loadOptions();
+
+}
+
+function loadOptions(){
+
+    //Carrega as opções do checkbox
+
+    //Busca se algum checkbox possui o atributo "checked"
+    document.querySelectorAll('.input').forEach((item) => {
+
+        if(item.hasAttribute('checked')){
+
+
+
+        }
+
+    })
 
 }
